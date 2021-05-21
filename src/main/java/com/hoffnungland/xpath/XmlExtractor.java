d2574d1527f2858f79145b58db08047b937ad42f
Speranza, Manuel M <manuel.speranza@webresults.it>
2021-05-21 14:33:04 +0200

    upgrade Saxon-HE and introduce extract sub-nodes values

--- src/main/java/com/hoffnungland/xpath/XmlExtractor.java
+++ src/main/java/com/hoffnungland/xpath/XmlExtractor.java
@@ -18,6 +18,7 @@ import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.SaxonApiUncheckedException;
 import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmItem;
 import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XdmValue;
 
@@ -29,15 +30,15 @@ import net.sf.saxon.s9api.XdmValue;
  * @since 29/06/2016
  */
 public class XmlExtractor {
-	
+
 	private static final Logger logger = LogManager.getLogger(XmlExtractor.class);
-	
+
 	private Processor xPathProcessor;
 	//private ItemTypeFactory itemTypeFactory;
 	//private XPathCompiler xPathCompiler;
 	private XmlNsCtx nsCtx;
 	private XdmNode docNode;
-	
+
 	/**
 	 * Default initializer of low-level components
 	 */
@@ -48,7 +49,7 @@ public class XmlExtractor {
 		this.nsCtx = new XmlNsCtx(this.xPathProcessor.newXPathCompiler());
 		logger.traceExit();
 	}
-	
+
 	/**
 	 * Initialize the low-level components starting from a File
 	 * @param file The XML file
@@ -86,7 +87,7 @@ public class XmlExtractor {
 		this.init(new StreamSource(reader));
 		logger.traceExit();
 	}
-	
+
 	/**
 	 * Initialize the low-level components starting from a W3C document source
 	 * @param source The W3C document source of an XML string
@@ -101,7 +102,7 @@ public class XmlExtractor {
 		this.docNode = documentBuilder.build(source);
 		logger.traceExit();
 	}
-	
+
 	public String extractString(String xPathStr, String xmlNs) throws SaxonApiException  {
 		logger.traceEntry();
 		logger.debug("Evaluating xpath " + xPathStr + " " + xmlNs);
@@ -110,7 +111,7 @@ public class XmlExtractor {
 			this.nsCtx.addNamespace(xmlNs);
 		}
 		XPathSelector xPathSelect = this.nsCtx.compileXPath(xPathStr);
-		xPathSelect.setContextItem(docNode);
+		xPathSelect.setContextItem(this.docNode);
 		XdmValue nodeValues = xPathSelect.evaluate();
 		if (nodeValues.size() > 0) {
 			return logger.traceExit(nodeValues.itemAt(0).getStringValue());
@@ -118,20 +119,62 @@ public class XmlExtractor {
 			return logger.traceExit((String) null);
 		}
 
-		
+
+	}
+
+	public String extractString(XdmItem nodeItem, String xPathStr, String xmlNs) throws SaxonApiException  {
+		logger.traceEntry();
+		if(nodeItem.isEmpty() || !nodeItem.isNode()) {
+			return logger.traceExit((String) null);
+		}
+
+		XdmNode xPathNode = (XdmNode) nodeItem;
+		logger.debug("Evaluating xpath " + xPathStr + " " + xmlNs);
+		//this.xpath.compile(xPathStr);
+		if (xmlNs != null && !xmlNs.isEmpty()) {
+			this.nsCtx.addNamespace(xmlNs);
+		}
+		XPathSelector xPathSelect = this.nsCtx.compileXPath(xPathStr);
+		xPathSelect.setContextItem(xPathNode);
+		XdmValue nodeValues = xPathSelect.evaluate();
+		if (nodeValues.size() > 0) {
+			return logger.traceExit(nodeValues.itemAt(0).getStringValue());
+		} else {
+			return logger.traceExit((String) null);
+		}
+
+
 	}
 	
 	public XdmValue extractNode(String xPath, String xmlNs) throws SaxonApiException, IndexOutOfBoundsException, SaxonApiUncheckedException, SAXException, IOException, ParserConfigurationException  {
 		logger.traceEntry();
 		logger.debug("Evaluating xpath " + xPath + " " + xmlNs);
 		//this.xpath.compile(xPathStr);
-			if (xmlNs != null && !xmlNs.isEmpty()) {
-				this.nsCtx.addNamespace(xmlNs);
-			}
-			XPathSelector xPathSelect = this.nsCtx.compileXPath(xPath);
-			
-			xPathSelect.setContextItem(docNode);
-			return logger.traceExit(xPathSelect.evaluate());
+		if (xmlNs != null && !xmlNs.isEmpty()) {
+			this.nsCtx.addNamespace(xmlNs);
+		}
+		XPathSelector xPathSelect = this.nsCtx.compileXPath(xPath);
+
+		xPathSelect.setContextItem(this.docNode);
+		return logger.traceExit(xPathSelect.evaluate());
+	}
+
+	public XdmValue extractNode(XdmItem nodeItem, String xPath, String xmlNs) throws SaxonApiException, IndexOutOfBoundsException, SaxonApiUncheckedException, SAXException, IOException, ParserConfigurationException  {
+		logger.traceEntry();
+		if(nodeItem.isEmpty() || !nodeItem.isNode()) {
+			return logger.traceExit((XdmValue) null);
+		}
+
+		XdmNode xPathNode = (XdmNode) nodeItem;
+		logger.debug("Evaluating xpath " + xPath + " " + xmlNs);
+		//this.xpath.compile(xPathStr);
+		if (xmlNs != null && !xmlNs.isEmpty()) {
+			this.nsCtx.addNamespace(xmlNs);
+		}
+		XPathSelector xPathSelect = this.nsCtx.compileXPath(xPath);
+
+		xPathSelect.setContextItem(xPathNode);
+		return logger.traceExit(xPathSelect.evaluate());
 	}
 	
 }
